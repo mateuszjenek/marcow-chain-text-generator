e00fda5cb3d07fc8596b8d8a0ebaddba32942161
Mateusz Jenek <mateusz.jenek@intel.com>
2020-05-17 23:25:32 +0200

    Zycie jak z bajki, czy istnieje?

--- cli.py
+++ cli.py
@@ -1,11 +1,13 @@
 import argparse
-from marcow_chain.FirstOrderMarcowChain import FirstOrderMarcowChain
-from generator.TextGenerator import TextGenerator
+from model.IMarcowChain import IMarcowChain
+from model.MarcowChainFactory import MarcowChainFactory
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("-s", "--source", required=True, help="Text file with line-by-line sentences")
 args = arg_parser.parse_args()
 
-generator = TextGenerator(FirstOrderMarcowChain(), args.source)
-
+lines = open(args.source, encoding='utf8').read().splitlines()
+               
+generator = MarcowChainFactory().first_order()
+generator.feed(lines)
 print(generator.generate())
